3699ddb13858092619a6f6bb9f49b26b972723e8
eremkaralar <eremkaralar@hotmail.com>
2021-03-12 17:42:00 +0300

    First Commit

--- ToDoListingApp/ViewController.swift
+++ ToDoListingApp/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+   
+    let toDo: [String] = ["Camasir", "Bulasik", "Supurum", "Yoga", "Sebze Alma"]
+    
+   
+    let cellReuseIdentifier = "cell"
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+           super.viewDidLoad()
+           
+         
+           self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+           
+           tableView.delegate = self
+           tableView.dataSource = self
+       }
+       
+      
+       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return self.toDo.count
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           
+        
+        let cell:UITableViewCell = (self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell?)!
+           
+          
+           cell.textLabel?.text = self.toDo[indexPath.row]
+           
+           return cell
+       }
+       
+       func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+           print("Cell No: \(indexPath.row).")
+       }
+   }
