685e71248e35c68a91a04e554d5b35cbda4dabf4
eremkaralar <eremkaralar@hotmail.com>
2021-03-18 18:17:42 +0300

    OOP assimilation/Button Added

---
+++ ToDoListingApp/ToDos.swift
@@ -0,0 +1,18 @@
+//
+//  ToDos.swift
+//  ToDoListingApp
+//
+//  Created by Erem Karalar on 18.03.2021.
+//
+
+import Foundation
+import UIKit
+
+//  Object is created.
+
+struct ToDos{
+    let TaskName: String
+    let TaskDue: String
+    let TaskImage: UIImage
+    
+}
--- ToDoListingApp/ViewController.swift
+++ ToDoListingApp/ViewController.swift
@@ -8,13 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-   
-    var toDo: [String] = ["Camasir", "Bulasik", "Supurum", "Yoga", "Sebze Alma"]
-    var TaskDues = ["2021-03-13", "2021-03-13", "2021-03-13", "2021-03-13", "2021-03-14", "2021-03-15"]
-    //Images are so cute OMG!
-    var TaskImage = [UIImage(named:"washing-machine"),UIImage(named:"dishwasher"),UIImage(named:"vacuum"),UIImage(named:"yoga"),UIImage(named:"vegetables-basket")]
 
+//    Object Oriented Prog. Assimilation
+
+    var toDos: [ToDos] = [ToDos(TaskName: "Camasir", TaskDue: "2021-03-13", TaskImage:UIImage(named:"washing-machine")!),ToDos(TaskName: "Bulasik", TaskDue: "2021-03-13", TaskImage:UIImage(named:"dishwasher")!),ToDos(TaskName: "Supurum", TaskDue: "2021-03-14", TaskImage:UIImage(named:"vacuum")!),ToDos(TaskName: "Yoga", TaskDue: "2021-03-14", TaskImage:UIImage(named:"yoga")!),ToDos(TaskName: "Sebze Alma", TaskDue: "2021-03-15", TaskImage:UIImage(named:"vegetables-basket")!)]
     
    
     let cellReuseIdentifier = "cell"
@@ -41,17 +38,16 @@ class ViewController: UIViewController {
 extension ViewController : UITableViewDelegate,UITableViewDataSource{
     
      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         return toDo.count
+         return toDos.count
      }
      
      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 //Access to the properties!
         let cell = tableView.dequeueReusableCell(withIdentifier: "DemoTableViewCell",for:indexPath) as! DemoTableViewCell
          
-         cell.TaskName.text = "Task: " + toDo[indexPath.row]
-         cell.TaskDue.text = "Due: " + TaskDues[indexPath.row]
-        //Let them settle cutely ^^
-        cell.TaskImage.image = TaskImage[indexPath.row]
+        cell.TaskName.text = "Task: " + toDos[indexPath.row].TaskName
+        cell.TaskDue.text = "Due: " + toDos[indexPath.row].TaskDue
+        cell.TaskImage.image = toDos[indexPath.row].TaskImage
          
          return cell
      }
@@ -64,9 +60,8 @@ extension ViewController : UITableViewDelegate,UITableViewDataSource{
 
            if editingStyle == .delete {
 
-                toDo.remove(at: indexPath.row)
-                TaskDues.remove(at:indexPath.row)
-                TaskImage.remove(at:indexPath.row)
+                toDos.remove(at: indexPath.row)
+               
             
                tableView.deleteRows(at: [indexPath], with: .fade)
 
